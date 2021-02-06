d242151d553d417d4e557b81171738ce8ac58a6b
Jose Martinez <jym126@hotmail.com>
2021-02-06 12:16:24 +0100

    added log tag for debugging purpose

--- app/src/main/java/com/martinez/MainActivity.java
+++ app/src/main/java/com/martinez/MainActivity.java
@@ -3,35 +3,48 @@ package com.martinez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //Declaring variables for each componet
     private EditText book;
     private EditText chapter;
     private  EditText verse;
     private Button buton;
+    //Just for debugging purpose tag for logcat
+    private  static final String Tag = "Save button handler";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        book = (EditText)findViewById(R.id.book);
-        chapter = (EditText)findViewById(R.id.chapter);
-        verse = (EditText)findViewById(R.id.verse);
-        buton = (Button)findViewById(R.id.button);
+        //Relating variables to component in xml layout
+        book = findViewById(R.id.book);
+        chapter = findViewById(R.id.chapter);
+        verse = findViewById(R.id.verse);
+        buton = findViewById(R.id.button);
 
     }
     //Method for the button save
     public void Escritura(View view){
+        //Creating variables to store the editText components data
         String bookData = book.getText().toString();
         String chapterData = chapter.getText().toString();
         String verseData = verse.getText().toString();
 
+        //Log message to show in logcat during debugging
+        Log.d(Tag, "About to create intent with John 3:16");
+
+        //creating new Intent object to send data from this activity to screen activity
         Intent intent = new Intent(this, Screen.class);
+
+        //sending data with the intent
         intent.putExtra("bookData", bookData);
         intent.putExtra("chapterData", chapterData);
         intent.putExtra("verseData", verseData);
--- app/src/main/java/com/martinez/Screen.java
+++ app/src/main/java/com/martinez/Screen.java
@@ -4,28 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class Screen extends AppCompatActivity {
 
     private TextView myScreen;
+    //Just for debugging purpose tag for logcat
+    private  static final String Tag = "Receiving intent";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla);
-        myScreen = (TextView)findViewById(R.id.screen1);
+        myScreen = findViewById(R.id.screen1);
 
+        //Log message to show in logcat during debugging
+        Log.d(Tag, "Received intent with John 3:16");
+
+        //Receiving data with intent
         String bookData = getIntent().getStringExtra("bookData");
         String chapterData = getIntent().getStringExtra("chapterData");
         String verseData = getIntent().getStringExtra("verseData");
-        String data = "Your Scripture is ";
 
+        //Sending customized and variables data to the textView (screen)
         myScreen.setText("This is your scripture \n" + bookData +" " + chapterData + ":" + verseData);
     }
 
-    //Method for return button
+    //Method for return to main activity button
     public void Returning(View view) {
         Intent returning = new Intent(Screen.this, MainActivity.class);
         startActivity(returning);
